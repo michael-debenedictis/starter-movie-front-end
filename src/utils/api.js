7892e5f0a8a60b38e343cbaaccc71abf2b0c7361
Michael DeBenedictis <mikedebenedictis@yahoo.com>
2022-09-26 15:56:14 -0400

    reverted

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,13 +1,6 @@
-require('dotenv').config()
-const result = dotenv.config()
-
-if (result.error) {
-  throw result.error
-}
-
-console.log(result.parsed)
 const API_BASE_URL =
   process.env.REACT_APP_API_BASE_URL
+
 /**
  * Defines the default headers for these functions to work with `json-server`
  */
