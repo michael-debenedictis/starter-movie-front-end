bb8592f6e455fffac0df9c54f8b06e56089a287d
Michael DeBenedictis <mikedebenedictis@yahoo.com>
2022-09-26 16:00:28 -0400

    fixed url

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,5 @@
 const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || 'https://limitless-citadel-48838.herokuapp.com/'
+  process.env.REACT_APP_API_BASE_URL || 'https://limitless-citadel-48838.herokuapp.com'
 
 /**
  * Defines the default headers for these functions to work with `json-server`
