f4cba870544e2fc985642960a5661eca0b10ffba
Michael DeBenedictis <mikedebenedictis@yahoo.com>
2022-09-26 14:58:30 -0400

    env created

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || "http://localhost:5000";
-
+  process.env.REACT_APP_API_BASE_URL
+console.log(process.env)
 /**
  * Defines the default headers for these functions to work with `json-server`
  */
